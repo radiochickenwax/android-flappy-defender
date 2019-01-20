001ecc83aa467cb930cb20ad713997230cbabfcc
Greg Ubuntu Chroot ZX551ml <gregorio.jp.mitchell@gmail.com>
2019-01-20 09:44:49 -0700

    added missing run() method to TDView and build now works

--- src/a/b/project/TDView.java
+++ src/a/b/project/TDView.java
@@ -1,12 +1,21 @@
 package a.b.project;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.Button;
-import android.view.View;
-import android.app.Activity;
-import android.content.Intent;
+/*
+The Context parameter that is passed into our constructor is a reference to the
+current state of our application within the Android system that is held by our
+GameActivity class. 
+*/
+import android.content.Context;
+import android.view.SurfaceView;
 
-public class TDView extends SurfaceView  {
-    
+/*TDView.java:6: error: TDView is not abstract and does not override abstract method run() in Runnable*/
+public class TDView extends SurfaceView implements Runnable {
+              public TDView(Context context) {
+              super(context);
+          }
+
+    @Override
+    public void run() {
+    }
+
 }
