ba4842c3dfd9edf1eb5c8d340380c362230a6ecc
Greg Ubuntu Chroot ZX551ml <gregorio.jp.mitchell@gmail.com>
2019-01-20 10:17:55 -0700

    fleshed out GameActivity

--- src/a/b/project/GameActivity.java
+++ src/a/b/project/GameActivity.java
@@ -2,11 +2,34 @@ package a.b.project;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.view.View;
-import android.app.Activity;
-import android.content.Intent;
+
 
 public class GameActivity extends Activity  {
+
+    // Our object to handle the View
+    private TDView gameView;
+
+    
+    // This is where the "Play" button from HomeActivity sends us
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+    }
+
+    // If the Activity is paused make sure to pause our thread
+    @Override
+    protected void onPause() {
+	super.onPause();
+	gameView.pause();
+    }
+
+    // If the Activity is resumed make sure to resume our thread
+    @Override
+    protected void onResume() {
+	super.onResume();
+	gameView.resume();
+    }
+
+
     
 }
