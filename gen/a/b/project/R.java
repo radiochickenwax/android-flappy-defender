dddb8858cc8751922b42fed3af88e9640fb550d7
Greg Ubuntu Chroot ZX551ml <gregorio.jp.mitchell@gmail.com>
2019-01-20 10:43:56 -0700

    fleshed out playership.java

--- gen/a/b/project/R.java
+++ gen/a/b/project/R.java
@@ -13,6 +13,7 @@ public final class R {
     public static final class drawable {
         public static final int back0=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int ship=0x7f020002;
     }
     public static final class id {
         public static final int buttonPlay=0x7f050001;
