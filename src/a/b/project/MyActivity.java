9692389ae6d9f75f75347389938b2060704fc5c1
Greg Ubuntu Chroot ZX551ml <gregorio.jp.mitchell@gmail.com>
2019-01-19 23:37:13 -0700

    added an onclicklistener to the button from the activity

--- src/a/b/project/MyActivity.java
+++ src/a/b/project/MyActivity.java
@@ -2,8 +2,10 @@ package a.b.project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
-public class MyActivity extends Activity{
+public class MyActivity extends Activity implements View.OnClickListener {
 
     // This is the entry point to our game
     @Override
@@ -13,5 +15,15 @@ public class MyActivity extends Activity{
 	//Here we set our UI layout as the view
 	setContentView(R.layout.main);
 
+	// Get a reference to the button in our layout
+	final Button buttonPlay = (Button)findViewById(R.id.buttonPlay);
+	// Listen for clicks
+	buttonPlay.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+      //Our code goes here
+    }
+    
 }
