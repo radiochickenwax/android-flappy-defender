049f8a7f1c3f45d871fc3687851325409341a8d8
Greg Ubuntu Chroot ZX551ml <gregorio.jp.mitchell@gmail.com>
2019-01-19 23:02:25 -0700

    added button and rebuilt

--- bin/classes.dex.d
+++ bin/classes.dex.d
@@ -1,5 +1,6 @@
 /cf/home/notes/android/sketches/2019-01-15/flappy-defender/bin/classes.dex : \
 /cf/home/notes/android/sketches/2019-01-15/flappy-defender/bin/classes/a/b/project/R$layout.class \
+/cf/home/notes/android/sketches/2019-01-15/flappy-defender/bin/classes/a/b/project/R$id.class \
 /cf/home/notes/android/sketches/2019-01-15/flappy-defender/bin/classes/a/b/project/MyActivity.class \
 /cf/home/notes/android/sketches/2019-01-15/flappy-defender/bin/classes/a/b/project/R$string.class \
 /cf/home/notes/android/sketches/2019-01-15/flappy-defender/bin/classes/a/b/project/BuildConfig.class \
