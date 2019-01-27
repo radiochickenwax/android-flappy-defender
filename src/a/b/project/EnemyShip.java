75f5237ceffb6483a99403fbafe49ae2a3f9185e
Greg Ubuntu Chroot ZX551ml <gregorio.jp.mitchell@gmail.com>
2019-01-26 20:06:52 -0700

    added EnemyShip.java

---
+++ src/a/b/project/EnemyShip.java
@@ -0,0 +1,14 @@
+
+   public class EnemyShip{
+       private Bitmap bitmap;
+       private int x, y;
+       private int speed = 1;
+
+        // Detect enemies leaving the screen
+        private int maxX;
+        private int minX;
+
+        // Spawn enemies within screen bounds
+        private int maxY;
+        private int minY;
+   }
